72b869b222ac5dadf4ffe0b5cb4e72f61d8fbec3
Amoxe200 <aimanaqari@gmail.com>
2025-05-31 19:30:31 +0100

    clearing

--- lib/services/base_trade.dart
+++
@@ -1,9 +0,0 @@
-import '../models/trade.dart';
-
-abstract class BaseTradeDB{
-  Future<void> insertTrade(Trade trade);
-  Future<void> init();  // Add this line
-  Future<List<Trade>> trades();
-  Future<void> updateTrade(Trade trade);
-  Future<void> deleteTrade(Trade trade);
-}
--- lib/services/trade_db_factory_web.dart
+++
@@ -1,4 +0,0 @@
-import 'package:trading_journal/services/trade_db_sembast.dart';
-import 'base_trade.dart';
-
-BaseTradeDB getTradeDB() => TradeDBSembast();
